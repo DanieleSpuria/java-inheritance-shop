abc6553af0e92e04c93b6ae279aa1ba7d5d3ae8d
DanieleSpuria <123416873+DanieleSpuria@users.noreply.github.com>
2023-09-18 15:35:06 +0200

    add Televisore

--- src/org/java/shop/Main.java
+++ src/org/java/shop/Main.java
@@ -6,5 +6,8 @@ public class Main {
 		Prodotto prod = new Prodotto("01", "coso", "deiCosi", 10, 20);
 		System.out.println(prod.prezzoIva());
 		
+		Smartphone sma = new Smartphone(
+				"02", "telefono", "di marca", 200, 10, "09575", 250);
+		System.out.println(sma.prezzoIva());
 	}
 }
---
+++ src/org/java/shop/Televisore.java
@@ -0,0 +1,38 @@
+package org.java.shop;
+
+public class Televisore extends Prodotto{
+	private String dimensioni;
+	private boolean smart;
+	
+	public Televisore
+		(String codice, String nome, String marca, float prezzo, float iva,
+		 String dimensioni, boolean smart) {
+		
+		super(codice, nome, marca, prezzo, iva);
+		
+		setDimensioni(dimensioni);
+		setSmart(smart);
+	}
+	
+	
+	// GETTER //
+	
+	public String getDimensioni() {
+		return dimensioni;
+	}
+	
+	public boolean getSmart() {
+		return smart;
+	}
+	
+	
+	// SETTER //
+	
+	public void setDimensioni(String dimensioni) {
+		this.dimensioni = dimensioni;
+	}
+	
+	public void setSmart(boolean smart) {
+		this.smart = smart;
+	}
+}
