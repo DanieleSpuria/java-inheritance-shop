e133066f9c8129d67060bc605a824dac8513d2fc
DanieleSpuria <123416873+DanieleSpuria@users.noreply.github.com>
2023-09-18 15:23:28 +0200

    add Smartphone

---
+++ src/org/java/shop/Smartphone.java
@@ -0,0 +1,38 @@
+package org.java.shop;
+
+public class Smartphone extends Prodotto {
+	private String imei;
+	private int memoria;
+	
+	public Smartphone 
+		(String codice, String nome, String marca, float prezzo, float iva,
+		 String imei, int memoria) {
+		
+		super(codice, nome, marca, prezzo, iva);
+		
+		setImei(imei);
+		setMemoria(memoria);
+	}
+	
+	
+	// GETTER //
+	
+	public String getImei() {
+		return imei;
+	}
+	
+	public int getMemoria() {
+		return memoria;
+	}
+	
+	
+	// SETTER //
+	
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+	
+	public void setMemoria(int memoria) {
+		this.memoria = memoria;
+	}
+}
